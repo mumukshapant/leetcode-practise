72b51ce48df28127cbeae8ab0b3e05cc4bc2de67
Mumuksha Pant <mumukshapan97@gmail.com>
2026-02-11 10:49:28 -0800

    Time: 362 ms (58.17%), Space: 12.4 MB (84.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,30 +5,66 @@ class Solution(object):
         :rtype: str
         """
 
-        l,r =0 , 0 
         n = len(s)
-        res=[]
+        start = 0
+        max_len = 0
 
-        for i in range (n): 
-            l=i
-            r=i+1
+        for i in range(n):
 
-            while l>=0 and r<n and s[l]==s[r]:
+            # ----- Odd length -----
+            l = r = i
 
-                if len(s[l:r+1]) > len(res):
-                    res = s[l:r+1]
-                l-=1
-                r+=1
+            while l >= 0 and r < n and s[l] == s[r]:
+                curr_len = r - l + 1
+                if curr_len > max_len:
+                    start = l
+                    max_len = curr_len
+                l -= 1
+                r += 1
+
+            # ----- Even length -----
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                curr_len = r - l + 1
+                if curr_len > max_len:
+                    start = l
+                    max_len = curr_len
+                l -= 1
+                r += 1
+
+        return s[start:start + max_len]
+
+
+
+
+
+
+
+
+        # l,r =0 , 0 
+        # n = len(s)
+        # res=[]
+
+        # for i in range (n): 
+        #     l=i
+        #     r=i+1
+
+        #     while l>=0 and r<n and s[l]==s[r]:
+
+        #         if len(s[l:r+1]) > len(res):
+        #             res = s[l:r+1] # slicing is very expensive , because it is creating a new string everytime. 
+        #         l-=1
+        #         r+=1
             
 
-            l=i
-            r=i
-            while l>=0 and r<n and s[l]==s[r]: 
-                if len(s[l:r+1])> len(res): 
-                    res = s[l:r+1]
-                l-=1
-                r+=1
-        return res
+        #     l=i
+        #     r=i
+        #     while l>=0 and r<n and s[l]==s[r]: 
+        #         if len(s[l:r+1])> len(res): 
+        #             res = s[l:r+1]
+        #         l-=1
+        #         r+=1
+        # return res
 
 
         

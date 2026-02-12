282f46bbb405e626184d9bf94f910ee97c067353
Mumuksha Pant <mumukshapan97@gmail.com>
2026-02-12 13:01:42 -0800

    Time: 6 ms (60.68%), Space: 12.5 MB (81.2%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -8,13 +8,17 @@ class Solution(object):
         
         logs.sort(key=lambda x : x[0])
 
-        adj = defaultdict(set)
-
+        #adj = defaultdict(set)
+        adj = [None] * n  # Use list instead of defaultdict for faster access
+        
         for i in range(n): 
             # every one is aquainted with themself 
             adj[i] = {i}
 
         for t,a,b in logs: 
+            if adj[a]==adj[b]: # This is to make it efficient, we don't have to check it again and again if adj of 2 nodes is same, eg 2 : {2,3, 4} and 4 : {2,3,4}
+                continue
+
             union_set = adj[a].union(adj[b]) # using union because when a is connected to b, it means we get all friends of b also. 
 
             # Update ALL people in merged group to point to same set
@@ -23,7 +27,7 @@ class Solution(object):
         
         
         # Check if everyone is now connected
-            if len(union_set) == n:
+            if len(adj[a]) == n: # early termination, we can also use len(union_set)
                 return t
         
         # Not everyone became connected

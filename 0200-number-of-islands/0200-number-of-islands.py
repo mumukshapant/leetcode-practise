722d35b2f9d7926040ac617cd24cd1f685ddfc21
Mumuksha Pant <mumukshapan97@gmail.com>
2026-02-17 16:04:52 -0800

    Time: 235 ms (46.5%), Space: 27.6 MB (89.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,33 +4,26 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        m = len(g)
-        n = len(g[0])
-        count=0
+
+        m,n = len(g), len(g[0])
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
-        
+        count=0 
 
-
-        def dfs(r, c): 
-            if (
-                r>=m or c>=n or r<0 or c<0 
-                or g[r][c]=='2'
-                or g[r][c]!='1'
-            ): 
+        def dfs(r,c): 
+            if r>=m or c>=n or r<0 or c<0 or g[r][c]!="1" : 
                 return 0
-
-            g[r][c]='2' #visited 
-            for d in directions: 
-                newrow = d[0]+r
-                newcol = d[1]+c
-
+            
+            g[r][c] = "2" # visited
+            for dr, dc in directions: 
+                newrow , newcol = dr+r , dc+c 
                 dfs(newrow, newcol)
-            return count
+            
+            return count 
+                
 
         for i in range(m): 
-            for j in range(n): 
-                if g[i][j]=="1": 
+            for j in range(n):
+                if g[i][j]=="1":  
                     dfs(i, j)
-                    count+=1 
+                    count+=1
         return count
-        

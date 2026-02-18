1576e249941d0cf2e623703e3de9d4a8d85e9058
Mumuksha Pant <mumukshapan97@gmail.com>
2026-02-17 16:47:53 -0800

    Time: 31 ms (57.76%), Space: 15.3 MB (37.31%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,30 +4,29 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        m= len(g)
-        n= len(g[0])
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        
-        maxarea=0
+
+        m,n = len(g), len(g[0])
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+    
+        maxarea= 0
 
         def dfs(r,c): 
             
-            if r>=m or c>=n or r<0 or c<0 or g[r][c]!=1: 
+            if r>=m or c>=n or r<0 or c<0 or g[r][c]!=1 : 
                 return 0
+            
             area=1
-            g[r][c]= 2
-            for d in directions: 
-                newrow = r+d[0]
-                newcol = c+d[1]
+            g[r][c] = 2 # visited
+
+            for dr, dc in directions: 
+                newrow , newcol = dr+r , dc+c 
                 area+=dfs(newrow, newcol)
+                
             return area
-
+        
         for i in range(m): 
-            for j in range(n): 
-                maxarea= max(maxarea, dfs(i,j) )
+            for j in range(n):
+                if g[i][j]==1:  
+                    maxarea= max( dfs(i, j), maxarea)
                     
-
         return maxarea
-
-            
-            

624cfc7c5dd5dad43fe590369ea0aee22d5c8db9
Mumuksha Pant <mumukshapan97@gmail.com>
2026-03-07 14:00:05 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -5,34 +5,33 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        
         adj = defaultdict(list)
-        visited = set()
 
-        for course,pre in prerequisites: 
-            adj[course].append(pre)
+        for c,p in prerequisites: 
+            adj[c].append(p)
         
-        def dfs(course) :
+        visited = [False]*n
 
-            if course in visited: 
-                return False
-            
-            if adj[course]== [] : 
+        def dfs(course): 
+            if adj[course]==[] : # no prerequisite
                 return True 
             
-            visited.add(course)
-
-            for p in adj[course]: 
-                if dfs(p) ==False : 
-                    return False
+            if course in visited : 
+                return False 
             
-            visited.remove(course)
-            adj[course]=[] # memoization -- to avoid repeated work. Eg  1 - [0,3]   2- [3,5]
-            # We would explore 3 two times. 
+            visited.append(course)
 
-            return True 
+            for pre in adj[course]:
+                if not dfs(pre ) :
+                    return False     
         
+            visited[course]= False 
+            adj[course]= []
+            return True 
+
         for c in range(n): 
-           if not dfs(c): 
-                return False
-        return True
+            if not dfs(c):
+                return False 
+        return True 
+
+        

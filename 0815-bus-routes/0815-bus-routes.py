feecaf165a02f65a7ce620e45abae19fbc32e346
Mumuksha Pant <mumukshapan97@gmail.com>
2026-03-27 11:56:01 -0700

    Time: 153 ms (44.67%), Space: 38.1 MB (38.07%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -6,44 +6,35 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
-        
-        # BFS -- because finding the shortest distance ( bfs on an undirected graph gives shortest possible path)
         if s==t: 
-            return 0 
+            return 0
 
-        adj= defaultdict(set)
-        q = deque([(s,0)]) # source, number_of_bus_taken
+        adj= defaultdict(list)
+        q= deque([(s,0)]) # source, number of bus taken 
+        
+        visited_bus= set()
+        visited_stop = set() 
 
-        visited_bus = set() 
-        visited_stop = set()
-
-        for bus, route in enumerate(routes): 
-              # 0, [1,2,7]
-              for stop in route: 
-                adj[stop].add(bus)
+        for bus, route in enumerate(routes):  # enum turns to (index, element)  
+            # 0,[1,2,7]
+            # 1, [3,6,7]
+            for stop in route: 
+                adj[stop].append(bus)
         
         while q: 
-            currstop, route_len = q.popleft() 
-            if currstop==t: 
+            currstop , route_len = q.popleft() # 1 , 0 
+            if currstop == t: 
                 return route_len
             
-            for nei_bus in adj[currstop]: 
+            for nei_bus in adj[currstop]:  # currstop=1 , nei_bus = 0
                 if nei_bus not in visited_bus: 
                     visited_bus.add(nei_bus)
 
-                    for currstop in routes[nei_bus]:
-                        if currstop not in visited_stop: 
+                    for currstop in routes[nei_bus] : # currstop =1 in routes[0]
+                        if currstop not in visited_stop : 
                             visited_stop.add(currstop)
                             q.append((currstop, route_len+1))
         
         return -1 
-
-
-            
-
-        
-
-
 
 

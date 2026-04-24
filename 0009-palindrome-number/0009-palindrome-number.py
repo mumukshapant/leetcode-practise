a77a7275af5a21ef8188c5da9da156bad18680fa
Mumuksha Pant <mumukshapan97@gmail.com>
2026-04-24 14:25:49 -0400

    Time: 16 ms (29.98%), Space: 12.3 MB (90.77%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,14 +4,11 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-    # O(log n )-- reversing half the number
-
-        if x<0 or (x % 10 == 0 and x != 0): 
+        if x<0 or ( x%10==0 and x!=0 ) : 
             return False
-        
-        rev=0
+        rev=0 
         while x> rev: 
-            rem=x%10
-            rev= rev*10 + rem
-            x=x//10
-        return x==rev or x==rev//10        
+            rem = x%10 
+            rev = rev*10 + rem 
+            x=x/10 
+        return x==rev or x== rev/10 

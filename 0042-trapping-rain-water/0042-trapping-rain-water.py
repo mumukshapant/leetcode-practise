846b8be09de440f2bd8d1654669fc77931bbd4d4
Mumuksha Pant <mumukshapan97@gmail.com>
2026-04-27 17:17:20 -0400

    Time: 11 ms (55.82%), Space: 13.4 MB (59.2%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,48 +1,22 @@
 class Solution(object):
-    def trap(self, height):
+    def trap(self, nums):
         """
         :type height: List[int]
         :rtype: int
         """
-        n = len(height)
+        n = len(nums)
         left=0
         right= n-1 
-
-        leftmax, rightmax = height[left], height[right]
+        leftmax,rightmax=nums[0], nums[n-1]
         area=0 
 
         while left<right: 
             if leftmax<rightmax: 
-                left+=1 
-                leftmax = max(leftmax, height[left])
-                area += leftmax - height[left]
+                left+=1
+                leftmax = max(nums[left], leftmax)
+                area += leftmax - nums[left]
             else: 
                 right-=1
-                rightmax = max(rightmax, height[right])
-                area+= rightmax - height[right]
-        return area
-
-                
-            
-
-
-
-
-        # leftmax = [0]*n
-        # rightmax = [0]*n 
-       
-
-        # leftmax[0] = height[0]
-        # rightmax[n-1] = height[n-1]
-
-        # for i in range(1,n): 
-        #     leftmax[i] = max(leftmax[i-1],height[i])
-        # for i in range(n-2, -1, -1): 
-        #     rightmax[i] = max(rightmax[i+1], height[i])
-        # ans=0
-        # res=0
-        # for i in range(n): 
-        #     ans += min(leftmax[i], rightmax[i]) - height[i]
-        # return ans 
-
-        
+                rightmax = max(nums[right], rightmax)
+                area += rightmax - nums[right]
+        return area 

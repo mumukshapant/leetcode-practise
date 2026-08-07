039787ca537fa61d34037f2b84ab9c0c68b6f054
Mumuksha Pant <mumukshapan97@gmail.com>
2026-08-07 16:22:33 -0400

    Time: 493 ms (10.96%), Space: 13.4 MB (22.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,20 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        i=0 
-        n=len(s)
-        charset = set()
-        j=0
+        myset = set() 
+        i,j=0,0 
         maxlen=0 
-        
-        while j<n: 
-            if s[j] not in charset : 
-                charset.add(s[j])
+        while j<len(s): 
+            if s[j] in myset: 
+                myset.remove(s[i])
+                i+=1
+            else: 
+
+                myset.add(s[j])
                 maxlen= max(j-i+1, maxlen)
                 j+=1
-            else: 
-                charset.remove(s[i])
-                i+=1
+            
         return maxlen
 
 

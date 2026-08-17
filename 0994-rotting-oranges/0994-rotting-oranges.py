217f7fd9d9ba270c52e3587fbbad0388dd70a96e
Mumuksha Pant <mumukshapan97@gmail.com>
2026-08-17 19:13:36 -0400

    Time: 3 ms (86.99%), Space: 12.5 MB (22.5%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,51 +1,40 @@
 class Solution(object):
-    def orangesRotting(self, g):
+    def orangesRotting(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        directions = [[1,0],[0,1],[-1,0],[0,-1]]
-        t=0 
-        m,n=len(g),len(g[0])
-        q=deque()
+        m,n = len(grid), len(grid[0])
+        q=deque() 
         fresh=0
+        time=0
 
+        directions= [[1,0],[-1,0],[0,1],[0,-1]]
 
-        for r in range(m): 
-            for c in range(n): 
-                if g[r][c]==2 : #rotten 
-                    q.append((r,c))
-                elif g[r][c]==1: 
-                    fresh+=1
-        
-        if fresh==0 :
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] ==2: # rotten 
+                    q.append((i,j))
+                elif grid[i][j]==1: #fresh 
+                    fresh+=1 
+                
+        if fresh==0 : 
             return 0 
-        
-    
-        while q and fresh>0 : 
+                    
+        while q and fresh>0 :
             for _ in range(len(q)): 
+                
                 r,c = q.popleft()
-                for dr, dc in directions: 
-                    newrow= dr+r
-                    newcol= dc+c 
 
-                    if newrow>=m or newcol>=n or newrow < 0 or newcol<0 or g[newrow][newcol]!=1 : 
+                for dr , dc in directions: 
+                    newrow, newcol = r+dr, c+dc 
+                    if newrow>=m or newcol>=n or newrow < 0 or newcol<0 or grid[newrow][newcol]!=1 : 
                         continue 
                     
-                    g[newrow][newcol]=2 #mark rotten 
+                    grid[newrow][newcol]= 2 # mark rotten 
                     q.append((newrow, newcol))
 
-                    fresh-=1 
-            t+=1
-
-    
-        return t if fresh == 0 else -1
-
-                
-
-
-        
-
-
+                    fresh-=1
+            time+=1
+        return time if fresh==0 else -1
             
-        
